db008ebc32bda9d39782ca17d31ea12e7ec9e3f7
HobbyisCoding <hobbyiscoding@Manojs-MacBook-Pro.local>
2017-09-24 18:24:25 +0530

    removed unwanted files

--- src/app/pipe/snakecase.pipe.spec.ts
+++ src/app/pipe/snakecase.pipe.spec.ts
@@ -1,20 +1,40 @@
+import { By } from '@angular/platform-browser';
+import { Component, DebugElement } from '@angular/core';
+import { selector } from 'rxjs/operator/publish';
+import { SnakeCasePipe } from './snakecase.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {SnakeCasePipe} from './snakecase.pipe';
+@Component({
+    selector: 'mock-component',
+    template: '<h1> {{message | snakecase}} </h1>'
+})
+export class MockComponent {
+    message = "App works";
+}
 
 describe(`it should test snake case pipe`, () => {
-    let snakecasePipe: SnakeCasePipe;
 
-    beforeEach( ()=> {
-        snakecasePipe = new SnakeCasePipe();
+    let mockFixture:ComponentFixture<MockComponent>;
+    let titleEL:DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations:[
+                MockComponent,
+                SnakeCasePipe
+            ]
+        }).compileComponents();
     });
 
-    it(`should convert the input with default char`, () => {
-        let message = snakecasePipe.transform('hi how are you');
-        expect(message).toEqual('hi-how-are-you');
+    beforeEach( () => {
+        mockFixture = TestBed.createComponent(MockComponent);
+        titleEL = mockFixture.debugElement.query(By.css('h1'));
+
     });
-   
-    it(`should convert the input with input char`, () => {
-        let message = snakecasePipe.transform('hi how are you', '#');
-        expect(message).toEqual('hi#how#are#you');
+
+    it(`should test snakecase pipe with test bed`, () => {
+        mockFixture.detectChanges();
+        let message = titleEL.nativeElement.textContent.trim();
+        expect(message).toEqual('App-works');
     });
 });
